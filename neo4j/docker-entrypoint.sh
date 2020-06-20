0aa120b1728e013cf3e78b491153e2e0c2c4f8d7
Davey Brown <>
2020-06-20 07:26:14 +0100

    cp certs

--- neo4j/docker-entrypoint.sh
+++ neo4j/docker-entrypoint.sh
@@ -11,10 +11,10 @@ sudo mkdir cluster
 sudo mkdir https
 
 for certsource in bolt cluster https ; do
-   sudo ln -s /var/lib/neo4j/certbot-live/fullchain.pem $certsource/neo4j.cert
-   sudo ln -s /var/lib/neo4j/certbot-live/privkey.pem $certsource/neo4j.key
+   sudo cp /var/lib/neo4j/certbot-live/fullchain.pem $certsource/neo4j.cert
+   sudo cp /var/lib/neo4j/certbot-live/privkey.pem $certsource/neo4j.key
    sudo mkdir $certsource/trusted
-   sudo ln -s /var/lib/neo4j/certbot-live/fullchain.pem $certsource/trusted/neo4j.cert ;
+   sudo cp /var/lib/neo4j/certbot-live/fullchain.pem $certsource/trusted/neo4j.cert ;
 done
 # Finally make sure everything is readable to the database
 sudo chgrp -R neo4j *
